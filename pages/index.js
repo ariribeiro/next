0142c0d895ab0dc099dce1f49f1135437b7df497
ariribeiro <arivaldoribeiro@gmail.com>
2020-10-27 14:23:13 -0300

    Segundo commit

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Qrcode generator</title>
+        <title>Qrcode new generator </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
